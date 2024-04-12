e3dace525ae9f3344f38c09b33889b7182511817
ochen011 <91513864+ochen011@users.noreply.github.com>
2024-04-12 19:45:50 +0000

    asdasd

--- gen.py
+++ gen.py
@@ -9,37 +9,36 @@ salt = '$1$zxHxP/cZ$'
 
 testPass = 'aabbbb'
 
-testHash = crypt.crypt(testPass, salt)
-
-print(testHash)
-
-givenPassHash = '$1$zxHxP/cZ$80hJZst.xa/IE2UbrZ/.m1'
+givenPassHash = '$1$zxHxP/cZ$9M9AoyLzpPza73./bvfsJ/'
 
 startTime = time.time()
 
 totalPassCalc = 0
 
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
-alphabet_starting_C = alphabet[alphabet.index('c'):]
-passwordLength = range(6, 7)
+# letGoThrough1 = ['e', 'g', 'i', 'k', 'm', 'o', 'q', 's']
+letGoThrough1 = ['v']
+letGoThrough2 = ['h', ' i', ' l', ' n', ' p', ' r', ' t', ' v', ' x', ' z']
+passwordLength = range(5, 6)
 
 startTime = time.time()
 
 totalPassCalc = 0
 
-for length in passwordLength: 
-    passwords = itertools.product(alphabet, repeat=length)
-    for password in passwords: 
-        totalPassCalc += 1
-        passwordStr = ''.join(password)
-        hashedPassToCrack = crypt.crypt(passwordStr, salt)
-        print('Pass: ', passwordStr, ' Hashed Test Pass: ', hashedPassToCrack, ' Given Pass Hash: ', givenPassHash)
-        if hashedPassToCrack == givenPassHash: 
-            print("Password cracked: ", hashedPassToCrack)
-            foundPass = passwordStr
-            break
-    else:
-        print('Pass Not Found')
+for lets in letGoThrough1: 
+    for length in passwordLength: 
+        passwords = itertools.product(alphabet, repeat=length)
+        for password in passwords: 
+            totalPassCalc += 1
+            passwordStr = lets + ''.join(password)
+            hashedPassToCrack = crypt.crypt(passwordStr, salt)
+            print('Pass: ', passwordStr, ' Hashed Test Pass: ', hashedPassToCrack)
+            if hashedPassToCrack == givenPassHash: 
+                print("Password cracked: ", hashedPassToCrack)
+                foundPass = passwordStr
+                break
+        else:
+            print('Pass Not Found')
 
 endTime = time.time() 
 
